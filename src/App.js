7145599d4267e5a990b1e81191215d5c222813fb
Landon Turner <lturner@LTurner-MBP.lan1>
2020-02-18 16:23:16 -0500

    pages show up on page

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ Look at each file to see where you need to pass props or add code
 import React from "react";
 import "./App.css";
 // import the PostsPage and SearchBar and add them to the App
-import SearchBarContainer from "./components/SearchBar/SearchBarContainer";
+import SearchBar from "./components/SearchBar/SearchBarContainer";
 import PostsPage from "./components/PostsContainer/PostsPage";
 
 
@@ -16,7 +16,7 @@ const App = () => {
   return (
     <div className="App">
       {/* Add imported components here to render them */}
-      <SearchBarContainer />
+      <SearchBar />
       <PostsPage />
     </div>
   );
--- src/components/PostsContainer/Post.js
+++ src/components/PostsContainer/Post.js
@@ -1,5 +1,5 @@
 // You will add code in this file
-import React from "react";
+import React, {useState} from "react";
 import CommentSection from "../CommentSection/CommentSectionContainer";
 import LikeSection from "./LikeSection";
 import PostHeader from "./PostHeader";
@@ -8,8 +8,8 @@ import "./Posts.css";
 
 // pass props in this file to
 const Post = props => {
+  console.log(props.postProps)
   // set up state for the likes
-
   return (
     <div className="post-border">
       <PostHeader
